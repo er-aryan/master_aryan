1bc3fb1441a68d6044f626fd3ada08fe8099b83b
er-aryan <72487339+er-aryan@users.noreply.github.com>
2020-10-11 00:53:20 +0530

    asdfgh
    fyligik.bkholij;lmnkljgyivb

---
+++ Untitled1.cpp
@@ -0,0 +1,106 @@
+/*# include<iostream>
+using namespace std;
+int main()
+{
+	int num1, num2;
+	cout<<"Enter the value of num1 :\n";
+	cin>>num1;
+	
+	cout<<"Enter the value of num2 :\n";
+	cin>>num2;
+	
+	cout<<"the sum is "<< num1+num2;
+	
+	return 0;
+}*/
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int a=7, b=5;
+	cout<<"operators in c++ :"<<endl;
+	cout<<"following are the types of operators in c++"<<endl;
+	cout<<"the value of a+b is: "<<a+b<<endl;
+	cout<<"the value of a-b is: "<<a-b<<endl;
+	cout<<"the value of a*b is: "<<a*b<<endl;
+	cout<<"the value of a/b is: "<<a/b<<endl;
+	cout<<"the value of a%b is: "<<a%b<<endl;
+	cout<<"the value of a++ is: "<<a++<<endl;
+	cout<<"the value of a-- is: "<<a--<<endl;
+	cout<<"the value of ++a is: "<<++a<<endl;
+	cout<<"the value of --a is: "<<--a<<endl;
+	
+	//Comparison operators
+	
+	cout<<"the value of a==b is: "<<(a==b)<<endl;
+	cout<<"the value of a!=b is: "<<(a!=b)<<endl;
+	cout<<"the value of a<=b is: "<<(a<=b)<<endl;
+	cout<<"the value of a>=b is: "<<(a>=b)<<endl;
+	cout<<"the value of a>b is: "<<(a>b)<<endl;
+	cout<<"the value of a<b is: "<<(a<b)<<endl;
+	
+	// logical operators
+	
+	cout<<"the value of and operator is: "<<((a==b) && (a<b))<<endl;
+	cout<<"the value of or operator is: "<<((a==b) || (a>b))<<endl;
+	cout<<"the value of not operator is: "<<!(a==b)<<endl;
+	return 0;
+}*/
+
+/*#include<iostream>
+
+using namespace std;
+int c = 45;
+int main()
+
+{
+/*	int a,b,c;
+	cout<<"the value of a is : "<<endl;
+	cin>>a;
+	
+	cout<<"the value of b is : "<<endl;
+	cin>>b;
+	
+	c = a+b;
+	cout<<"the sum of a and b is c : "<<c<<endl;
+	cout<<"the value global c is : "<<::c<<endl;            // " :: " this is the scope resolution operator..
+	return 0;*/
+
+	/*float d = 34.4f;
+	long double e = 34.5l;
+	cout<<"the value of d is :"<<d<<endl<<"the value of e is : "<<e<<endl;
+	return 0;*/
+	
+	//.............reference variable..............
+	//shyam das--------->shyam------------>shamu...
+	
+	/*float x = 350;
+	float & y = x;                                         // & ---> amperscend....
+	cout<<"the value of x is : "<<x<<endl;
+	cout<<"the value of y is : "<<y<<endl;
+	return 0;*/
+	
+	//............type casting variables..........
+	/*float a = 12;
+	float b = 12.35;
+	cout<<"the value of a is : "<<(float)a<<endl;
+	cout<<"the value of a is : "<<float(a)<<endl;
+	
+	cout<<"the value of b is : "<<(int)b<<endl;
+	cout<<"the value of b is : "<<int(b)<<endl;
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
